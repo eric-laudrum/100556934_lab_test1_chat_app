d770019545e498f7989ea6b58266d9607c4acbcd
eric-laudrum <ericlaudrum@gmail.com>
2026-02-11 10:51:19 -0500

    Add GroupMessage and PrivateMessage schemas

--- models/Users.js
+++ models/Users.js
@@ -2,16 +2,21 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
     username: {
-    type: String,
+        type: String,
         required: [true, "Username cannot be empty" ],
         trim: true,
         unique: true,
-  },
+    },
     firstname: {
         type: String,
-        required: [true, "Firstname cannot be empty" ],  //associated error message if the validation is violated
+        required: [true, "Firstname cannot be empty" ], 
         trim: true,
-  },
+    },
+    lastname: {
+        type: String,
+        required: [true, "Lastname cannot be empty" ],
+        trim: true,
+    },
     password: {
         type: String,
         required: [true, "Please enter a password"],
@@ -20,22 +25,13 @@ const UserSchema = new mongoose.Schema({
     createdOn: { 
         type: Date,
         default: Date.now
-  },
-
-  updatedOn: { 
+    },
+    updatedOn: { 
         type: Date,
         default: Date.now
-  },
+    },
 });
 
-//Declare Virtual Fields
-EmployeeSchema.virtual('fullname')
-  .get(function(){
-    return `${this.firstname} ${this.lastname}`
-    })
-  .set(function(value){
-    console.log('fullname set - ', value);
-    })
 
 module.exports = mongoose.model('User', UserSchema);
 
